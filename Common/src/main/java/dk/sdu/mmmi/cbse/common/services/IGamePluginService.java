f083d71b8d8b4b3ac4d41095518a55739f2f087f
Kristoffer <Kripe22@student.sdu.dk>
2024-05-19 21:35:08 +0200

    Small fix

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
@@ -5,16 +5,16 @@ import dk.sdu.mmmi.cbse.common.data.World;
 
 public interface IGamePluginService {
 
-    /*
+    void start(GameData gameData, World world);
+
+
+    void stop(GameData gameData, World world);
+}
+
+ /*
     Void start contains the gamedata and the world data on start of the engine running
 
     Pre-condition is None of the data can be null
 
     Post-condition is that the data has been applied to engine
      */
-
-    void start(GameData gameData, World world);
-
-
-    void stop(GameData gameData, World world);
-}
