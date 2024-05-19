ddf226de3d793f02591c754463e9d4d927e198b4
Kristoffer <Kripe22@student.sdu.dk>
2024-05-19 21:46:21 +0200

    Small fix

--- CommonBullet/src/main/java/dk/sdu/mmmi/cbse/common/bullet/BulletSPI.java
+++ CommonBullet/src/main/java/dk/sdu/mmmi/cbse/common/bullet/BulletSPI.java
@@ -3,6 +3,13 @@ package dk.sdu.mmmi.cbse.common.bullet;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 
+
+
+/**
+ * BulletSPI interface
+ */
+
+
 public interface BulletSPI {
     Entity createBullet(Entity e, GameData gameData);
 }
