99d9908b5b21febde36570525131d8a74830d5d6
Kristoffer <Kripe22@student.sdu.dk>
2024-05-20 16:54:23 +0200

    Small fix

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
@@ -3,12 +3,14 @@ package dk.sdu.mmmi.cbse.common.services;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
+public interface IPostEntityProcessingService {
+
+
+    void process(GameData gameData, World world);
+}
 /**
  *
  * @author jcs
  */
 
-public interface IPostEntityProcessingService {
 
-    void process(GameData gameData, World world);
-}
