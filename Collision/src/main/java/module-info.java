b53265712f31e69890d23d5b3850f32cd9b01170
Kristoffer <Kripe22@student.sdu.dk>
2024-05-21 02:28:25 +0200

    Testing Testing im just suggesting, you and I might not be the best thing exit, exit somehow I guess it right, RIGGGGHT. but I DONT WANT WANT YA, DON'T MEAN TO TAUNT YA,

--- Collision/src/main/java/module-info.java
+++ Collision/src/main/java/module-info.java
@@ -5,4 +5,6 @@ module Collision {
     requires java.desktop;
     requires Player;
     provides IPostEntityProcessingService with dk.sdu.mmmi.cbse.Collision.CollisionDetector;
+
+    exports dk.sdu.mmmi.cbse.Collision;
 }
